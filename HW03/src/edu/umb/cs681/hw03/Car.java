ed8f6313d1df1c31a8f889c9c7570fd2d17be25c
Jesse Freeman <jessewilliamfreeman@gmail.com>
2021-03-28 17:56:55 -0400

    HW03

---
+++ HW03/src/edu/umb/cs681/hw03/Car.java
@@ -0,0 +1,97 @@
+package edu.umb.cs681.hw03;
+
+import java.util.ArrayList;
+
+public class Car {
+	
+	private String make, model;
+	private int mileage, year, dominationCount;
+	private float price;
+	
+	public Car(
+		String make, 
+		String model,
+		int mileage, 
+		int year,
+		float price
+			) {
+		
+		this.make    = make;
+		this.model   = model;
+		this.mileage = mileage;
+		this.year    = year;
+		this.price   = price;
+		
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public int getMileage() {
+		return mileage;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+	
+	public int getDominationCount() {
+		return dominationCount;
+	}
+	
+	public void setDominationCount(ArrayList<Car> cars) {
+		
+		int dominationCount = 0;
+				
+		for(Car c: cars) {
+			
+			int yearC;
+			int mileC;
+			int priceC;
+			
+			if (this.getYear() > c.getYear()) {
+				yearC = 1;
+			} else if (this.getYear() == c.getYear()) {
+				yearC = 0;
+			} else {
+				yearC = -1;
+			}
+			
+			if (this.getMileage() > c.getMileage()) {
+				mileC = 1;
+			} else if (this.getMileage() == c.getMileage()) {
+				mileC = 0;
+			} else {
+				mileC = -1;
+			}
+			
+			if (this.getPrice() > c.getPrice()) {
+				priceC = 1;
+			} else if (this.getPrice() == c.getPrice()) {
+				priceC = 0;
+			} else {
+				priceC = -1;
+			}
+
+			
+			boolean notInferior = yearC >= 0 && mileC >= 0 && priceC >= 0;
+			boolean isSuperior  = yearC >  0 || mileC >  0 || priceC >  0;
+			
+			if (isSuperior && notInferior) {
+				dominationCount++;
+			}
+		}
+		
+		this.dominationCount = dominationCount;
+	}
+
+}
---
+++ HW03/test/src/edu/umb/cs681/hw03/CarTest.java
@@ -0,0 +1,213 @@
+package edu.umb.cs681.hw03;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import edu.umb.cs681.hw03.Car;
+
+class CarTest {
+	
+	
+	
+	private ArrayList<Car> makeUsedCars() {
+		
+		ArrayList<Car> usedCars = new ArrayList<Car>();
+		
+		usedCars.add(new Car("Toyota", "RAV4",     20000,  2019, 10000));
+		usedCars.add(new Car("Toyota", "Cool",     50000,  2017, 9000));
+		usedCars.add(new Car("Toyota", "RAV0",     5000,   2020, 20000));
+		usedCars.add(new Car("Toyota", "RAV1",     15000,  2018, 8000));
+		usedCars.add(new Car("Subaru", "TheBest",  100000, 2022, 1000000));
+		usedCars.add(new Car("Subaru", "TheWorst", 10,     1900, 1000));
+		
+		return usedCars;
+	}
+	
+	private String[] carToStringArray(Car car) {
+		String[] carArray = {car.getMake() , car.getModel(), Integer.toString(car.getYear())};
+		return carArray;
+	}
+
+	private String[] carListToMakeArray(ArrayList<Car> carList) {
+		ArrayList<String> result = new ArrayList<String>();
+		
+		for (Car c: carList) {
+			result.add(c.getModel());
+		}
+		
+		return result.toArray(new String[result.size()]);
+	}
+	
+	@Test
+	void verifyCarEqualityWithMakeModelYear() {
+		String[] expected = {"Toyota", "RAV4", "2018"};
+		Car actual = new Car("Toyota", "RAV4", 20000, 2018, 10000);
+		assertArrayEquals(expected,
+				carToStringArray(actual));
+	}
+	
+	@Test
+	void MileageComparatorTest() {
+		String[] expected = {
+				"TheWorst",
+				"RAV0",
+				"RAV1",
+				"RAV4",
+				"Cool",
+				"TheBest"
+		};
+		
+		ArrayList<Car> usedCars = makeUsedCars();
+		
+		usedCars = usedCars.stream()
+			.sorted(Comparator.comparing(Car::getMileage))
+			.collect(Collectors.toCollection(ArrayList<Car>::new));
+		
+		
+		String[] actual = carListToMakeArray(usedCars);
+		
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	void YearComparatorTest() {
+		String[] expected = {
+				"TheWorst",
+				"Cool",
+				"RAV1",
+				"RAV4",
+				"RAV0",
+				"TheBest"
+		};
+		
+		ArrayList<Car> usedCars = makeUsedCars();
+		
+		usedCars = usedCars.stream()
+			.sorted(Comparator.comparing(Car::getYear))
+			.collect(Collectors.toCollection(ArrayList<Car>::new));
+		
+		String[] actual = carListToMakeArray(usedCars);
+		
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	void PriceComparatorTest() {
+		String[] expected = {
+				"TheWorst",
+				"RAV1",
+				"Cool",
+				"RAV4",
+				"RAV0",
+				"TheBest"
+		};
+		
+		ArrayList<Car> usedCars = makeUsedCars();
+		
+		usedCars = usedCars.stream()
+			.sorted(Comparator.comparing(Car::getPrice))
+			.collect(Collectors.toCollection(ArrayList<Car>::new));
+		
+		String[] actual = carListToMakeArray(usedCars);
+		
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	void ParetoComparatorTest() {
+		String[] expected = {
+				"TheWorst",
+				"Cool",
+				"RAV0",
+				"RAV1",
+				"RAV4",
+				"TheBest"
+		};
+		
+		ArrayList<Car> usedCars = makeUsedCars();
+		
+		for(Car c: usedCars){
+			c.setDominationCount(usedCars); 
+		}
+		
+		usedCars = usedCars.stream()
+			.sorted(Comparator.comparing(Car::getDominationCount))
+			.collect(Collectors.toCollection(ArrayList<Car>::new));
+		
+		String[] actual = carListToMakeArray(usedCars);
+		
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	void carCountTest() {
+		int expected = 6;
+		
+		ArrayList<Car> usedCars = makeUsedCars();
+		
+		for(Car c: usedCars){
+			c.setDominationCount(usedCars); 
+		}
+		
+		int actual = usedCars.stream()
+			.reduce(0, (Integer result, Car car) -> ++result, (Integer finalResult, Integer intermediateResult) -> finalResult);
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void carMinTest() {
+		int expected = 10;
+		
+		ArrayList<Car> usedCars = makeUsedCars();
+		
+		for(Car c: usedCars){
+			c.setDominationCount(usedCars); 
+		}
+		
+		int actual = usedCars.stream()
+			.map((Car car) -> car.getMileage())
+			.reduce(Integer.MAX_VALUE, (Integer result, Integer carMileage) -> {
+				if (carMileage < result) {
+					return carMileage;
+				}
+				else {
+					return result;
+				}
+			});
+		
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	void carMaxTest() {
+		int expected = 100000;
+		
+		ArrayList<Car> usedCars = makeUsedCars();
+		
+		for(Car c: usedCars){
+			c.setDominationCount(usedCars); 
+		}
+		
+		int actual = usedCars.stream()
+			.map((Car car) -> car.getMileage())
+			.reduce(0, (Integer result, Integer carMileage) -> {
+				if (carMileage > result) {
+					return carMileage;
+				}
+				else {
+					return result;
+				}
+			});
+		
+		assertEquals(expected, actual);
+	}
+
+}
