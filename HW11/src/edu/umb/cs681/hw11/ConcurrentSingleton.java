c21ba2d369122c091b4acc4d22eae8dc4cdcbca5
Jesse Freeman <jessewilliamfreeman@gmail.com>
2021-05-20 22:29:24 -0400

    HW11

---
+++ HW11/src/edu/umb/cs681/hw11/ConcurrentSingleton.java
@@ -0,0 +1,32 @@
+package edu.umb.cs681.hw11;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ConcurrentSingleton {
+	private ConcurrentSingleton() {};
+	private static AtomicReference<ConcurrentSingleton> instance = new AtomicReference(null);
+	
+	public static ConcurrentSingleton getInstance() {
+		if (instance.get() == null) {
+			instance.set(new ConcurrentSingleton()); 
+		}
+		return instance.get();
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Two threads getting ConcurrentSingleton:");
+		Thread t1 = new Thread(() -> {
+			System.out.println(ConcurrentSingleton.getInstance());
+		});
+		Thread t2 = new Thread(() -> {
+			System.out.println(ConcurrentSingleton.getInstance());
+		});
+		t1.start();
+		t2.start();
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {}
+	}
+}
